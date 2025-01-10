7a7eeedceefd9ce21a15a0f9d747d6fc27a2c50d
Lucky Manikpuri <149005970+lucky001118@users.noreply.github.com>
2025-01-10 23:16:13 +0530

    Update EditUserProfileServiceApplication.java

--- EditUserProfileService/src/main/java/com/artgallery/edituserprofile/EditUserProfileServiceApplication.java
+++ EditUserProfileService/src/main/java/com/artgallery/edituserprofile/EditUserProfileServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EditUserProfileServiceApplication {
 
 	public static void main(String[] args) {
+		// Starting point 
 		SpringApplication.run(EditUserProfileServiceApplication.class, args);
 	}
 
